8755b612c7753bf60d15bf5761a0f59431eaeaca
istvanmo <mo.istvan@gmail.com>
2018-02-22 14:08:31 +0100

    added threaded data downloading

--- async_proba.py
+++ async_proba.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import async_timeout
+import os
 
 base_url = "https://api.hitbtc.com/api/2/public/ticker/"
 pair_list = ["ETHBTC", "BCHBTC", "DASHBTC", "XMRBTC", "XRPBTC", "LTCBTC", "BCNBTC", "ZECBTC",
@@ -8,6 +9,7 @@ pair_list = ["ETHBTC", "BCHBTC", "DASHBTC", "XMRBTC", "XRPBTC", "LTCBTC", "BCNBT
              "ARDRBTC"]
 url_list = [base_url + pair for pair in pair_list]
 
+"""
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
@@ -22,3 +24,23 @@ async def main(loop):
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main(loop))
+"""
+
+
+async def download_coroutine(session, url):
+    async with session.get(url) as response:
+        print(await response.content.read())
+        return await response.release()
+
+
+async def main(loop):
+    urls = url_list
+
+    async with aiohttp.ClientSession(loop=loop) as session:
+        tasks = [download_coroutine(session, url) for url in urls]
+        await asyncio.gather(*tasks)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))
+
